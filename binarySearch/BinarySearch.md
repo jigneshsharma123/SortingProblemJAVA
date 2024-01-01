b000ad9ccf63177151cb956c0fb48cc88cd1789d
jignesh sharma <jigneshsharma9868@gmail.com>
2024-01-01 09:18:48 +0530

    successfully solved the searching problem using binary search

---
+++ binarySearch/BinarySearch.md
@@ -0,0 +1,10 @@
+# What is Binary Search 
+```
+1. Binary Search is an Algorithm used for searching the element in the given set of the element. 
+2. there is a condition where we can use the binary search 
+    * Element must be already shorted(accending or decending order )
+
+Algorithm --> 
+a --> 
+
+```
--- binarySearch/Solution.java
+++ binarySearch/Solution.java
@@ -1,1 +1,25 @@
 package binarySearch;
+public class Solution {
+
+    public static int BinarySearch(int[] arr, int x) {
+        int len = arr.length;
+        int end = len - 1;
+        int start = 0; 
+        while(start <= end) {
+            int  mid = (start + end) / 2;
+            if(arr[mid] == x ) {
+                return mid + 1;
+            }
+            else if(arr[mid] < x) {
+               start = mid  + 1;
+            }else {
+              end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+       int element[] = {1,2,3,4,5,6};
+       System.out.println( BinarySearch(element,0));
+    }
+}
